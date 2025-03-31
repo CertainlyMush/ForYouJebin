2d41c0974fc0db2d3ba1338da490cc6d844e7db6
CertainlyMush <certainlymush@gmail.com>
2025-03-31 15:10:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const messages = [
+    "Jebin, are you sure?",
+    "Really sure??",
+    "Let's give it a try, just think about it!",
+    "Pookie please...",
+    "I'm serious about you!!!",
+    "If you say no, I'll be really sad..",
+    "Oke fine, I'll stop...",
+    "JK, I'LL NEVER GIVE UP ON YOU!"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
